d3266948d1556fd2f32917feb0cefd8114e04698
Amthebest2020 <75787915+Amthebest2020@users.noreply.github.com>
2021-04-06 19:10:46 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,19 @@
+class Paper{
+    constructor(x,y,r){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+        this.r = r;
+        this.body=Bodies.circle(x, y, r , options);
+ 		World.add(world, this.body);
+        this.image = loadImage("paper.png");
+    }
+    display(){
+        circle(0, 0 , this.r)
+        
+        image(this.image, 0, 0)
+    }
+}
